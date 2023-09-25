fede60694991f79bef359eeed10e594f422ccb5b
simoneeti <vintrob.simon@gmail.com>
2023-09-25 11:38:48 -0300

    Revert "first commit"
    This reverts commit 5d048e2633ff2d9fedab56cde3644b9235c30771.

--- tests/run.py
+++ tests/run.py
@@ -1,24 +1,15 @@
-import sys
-import os
 import unittest
-import sys
-
-from src.run import app
 
 class case(unittest.TestCase):
     def setUp(self) -> None:
         "Corre antes que todos los tests."
-        app.testing = True
-        self.app = app.test_client()
         pass
 
     def test1(self) -> None:
         """
-        Sample test para Flask.
+        Sample test.
         """
-        result = self.app.get("/")
-        print(result.get_json())
-        pass
+        self.assertEqual(1, 1)
 
 if __name__ == '__main__':
     unittest.main()
